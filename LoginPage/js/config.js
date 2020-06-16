4a81c24f48832d89ba037f1257fdf55dad4bca5f
chintapalli <sp.chintapalli@gmail.com>
2020-06-16 15:48:00 +0530

    first commit

---
+++ LoginPage/js/config.js
@@ -0,0 +1,9 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-south-1_yLiiooawx', // e.g. us-east-2_uXboG5pAb
+        region: 'ap-south-1', // e.g. us-east-2
+		clientId: '6p1vulmvhacuhhquptqgttplvs' //is this used anywhere?
+    },
+};
+
+
